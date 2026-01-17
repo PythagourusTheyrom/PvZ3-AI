3b64e04655c09cbf5a360fd20d677fcee48189ed
PythagourusTheyrom <ayaanmohammediqbal@gmail.com>
2026-01-17 17:16:10 +0530

    Auto-update: Sat Jan 17 17:16:10 IST 2026

--- internal/core/game.go
+++ internal/core/game.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"pvz/internal/assets"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
@@ -10,10 +12,7 @@ type Game struct {
 }
 
 func NewGame() *Game {
-	// Basic asset loading test
-	// In a real game, you might want to load assets asynchronously or during a loading screen
-	// For now, we load directly to verify.
-	// Note: We need to import the assets package, but first we need to fix the import in the file header
+	assets.LoadAssets()
 	return &Game{}
 }
 
@@ -23,11 +22,9 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Draw logic goes here
-	// Verify background drawing
-	// We will assume assets are loaded in main for now or we can call it here once (safeguard needed)
-	// Actually, let's just make NewGame load it, but I can't easily edit imports with this tool unless I replace the whole file or use multi_replace.
-	// I'll assume I update imports separately or simply rewrite the file since it's small.
+	if assets.BackgroundImage != nil {
+		screen.DrawImage(assets.BackgroundImage, nil)
+	}
 	ebitenutil.DebugPrint(screen, "PvZ Go - Early Alpha")
 }
 
