32b3ae9147f57a6dde5333698043939281b39635
PythagourusTheyrom <ayaanmohammediqbal@gmail.com>
2026-01-17 17:15:57 +0530

    Auto-update: Sat Jan 17 17:15:57 IST 2026

---
+++ internal/assets/assets.go
@@ -0,0 +1,42 @@
+package assets
+
+import (
+	"bytes"
+	"embed"
+	"image"
+	_ "image/png"
+	"log"
+
+	_ "golang.org/x/image/webp"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+//go:embed images/*.webp
+var assetsFS embed.FS
+
+var (
+	BackgroundImage *ebiten.Image
+)
+
+func LoadAssets() {
+	var err error
+	BackgroundImage, err = loadImage("images/background.webp")
+	if err != nil {
+		log.Fatalf("failed to load background: %v", err)
+	}
+}
+
+func loadImage(path string) (*ebiten.Image, error) {
+	data, err := assetsFS.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	img, _, err := image.Decode(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+
+	return ebiten.NewImageFromImage(img), nil
+}
