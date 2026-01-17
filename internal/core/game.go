5e517637683610c19bf1d73825711234e431058f
PythagourusTheyrom <ayaanmohammediqbal@gmail.com>
2026-01-17 17:16:00 +0530

    Auto-update: Sat Jan 17 17:16:00 IST 2026

--- internal/core/game.go
+++ internal/core/game.go
@@ -10,6 +10,10 @@ type Game struct {
 }
 
 func NewGame() *Game {
+	// Basic asset loading test
+	// In a real game, you might want to load assets asynchronously or during a loading screen
+	// For now, we load directly to verify.
+	// Note: We need to import the assets package, but first we need to fix the import in the file header
 	return &Game{}
 }
 
@@ -20,6 +24,10 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	// Draw logic goes here
+	// Verify background drawing
+	// We will assume assets are loaded in main for now or we can call it here once (safeguard needed)
+	// Actually, let's just make NewGame load it, but I can't easily edit imports with this tool unless I replace the whole file or use multi_replace.
+	// I'll assume I update imports separately or simply rewrite the file since it's small.
 	ebitenutil.DebugPrint(screen, "PvZ Go - Early Alpha")
 }
 
